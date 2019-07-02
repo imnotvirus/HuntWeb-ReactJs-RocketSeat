e66cddc9d278e26caba9d0376b884e90e0b53e1f
Luiz Claudio <contato.luizdevs@gmail.com>
2019-07-02 12:32:48 -0300

    commit final

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import "./style.css";
+import {Link} from "react-router-dom";
+import Routes from "../../routes.js"
+
 const Header = () => (
-    <header id="main-header"> JSHunt</header>
+    
+    <header id="main-header"> 
+        <a href="/">
+        JSHunt 
+        </a>
+    </header>
+    
 );
 
 export default Header;
