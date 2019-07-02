067a6b8abc908e1a1f8a84bd39c3403b7971c3d0
Luiz Claudio <contato.luizdevs@gmail.com>
2019-07-02 14:07:43 -0300

    Updates

---
+++ precache-manifest.9127c82ad7dc1fc0e0412b5fb00c6cae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f59998467170cbc85e865f94816272b",
+    "url": "/HuntWeb-ReactJs-RocketSeat/index.html"
+  },
+  {
+    "revision": "fa866a258644cd58bcea",
+    "url": "/HuntWeb-ReactJs-RocketSeat/static/css/main.3a745c63.chunk.css"
+  },
+  {
+    "revision": "4078a0d44ff8b6c184ae",
+    "url": "/HuntWeb-ReactJs-RocketSeat/static/js/2.f153c23e.chunk.js"
+  },
+  {
+    "revision": "fa866a258644cd58bcea",
+    "url": "/HuntWeb-ReactJs-RocketSeat/static/js/main.f23dc82e.chunk.js"
+  },
+  {
+    "revision": "1bd563c900fda702611e",
+    "url": "/HuntWeb-ReactJs-RocketSeat/static/js/runtime~main.3f163c25.js"
+  }
+]);
